d3aeede99f38bf09fea813288bc6668ab1450ed3
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-09-18 18:17:32 +0500

    Icons changed & added

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,34 @@ export default function Document() {
       <Head>
         <title>Code Brigade - Dasturchilar Brigadasi</title>
         <link rel="icon" href="/icon.png" sizes="any" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/icons/180.png"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/icons/32.png"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/icons/16.png"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/images/icons/192.png"
+        />
+
         <meta
           name="description"
           content="Kafolatlangan Natija va Sifat Uyg`unligi. Oliy sifatli xizmat, mutaxassislardan eksklyuziv tavsiyalar va professional koʻmak."
@@ -26,12 +54,16 @@ export default function Document() {
         <meta property="og:title" content="Kafolatlangan Natija va Sifat Uyg`unligi. Oliy sifatli xizmat, mutaxassislardan eksklyuziv tavsiyalar va professional koʻmak." />
         <meta property="og:description" content="Kafolatlangan Natija va Sifat Uyg`unligi. Oliy sifatli xizmat, mutaxassislardan eksklyuziv tavsiyalar va professional koʻmak." />
         <meta property="og:image" content="/images/site-img.png" />
+        <meta property="og:image:width" content="500" />
+        <meta property="og:image:height" content="293" />
 
-        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:card" content="/images/site-img.png" />
         <meta property="twitter:url" content="https://code-brigade.vercel.app/" />
         <meta property="twitter:title" content="Kafolatlangan Natija va Sifat Uyg`unligi. Oliy sifatli xizmat, mutaxassislardan eksklyuziv tavsiyalar va professional koʻmak." />
         <meta property="twitter:description" content="Kafolatlangan Natija va Sifat Uyg`unligi. Oliy sifatli xizmat, mutaxassislardan eksklyuziv tavsiyalar va professional koʻmak." />
         <meta property="twitter:image" content="/images/site-img.png" />
+        <meta property="twitter:image:width" content="500" />
+        <meta property="twitter:image:height" content="293" />
       </Head>
       <body>
         <Main />
